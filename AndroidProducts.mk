9caa608439735c7df71870f713352a893f78dda5
tilt-rgb <lesma_chan@pm.me>
2025-06-25 16:03:20 -0300

    m52xq: Init VoltageOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m52xq.mk
+    $(LOCAL_DIR)/voltage_m52xq.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_m52xq-userdebug \
-    lineage_m52xq-user \
-    lineage_m52xq-eng
+    voltage_m52xq-userdebug \
+    voltage_m52xq-user \
+    voltage_m52xq-eng
