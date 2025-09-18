5f54c77896e31cc9309e27e8c290e94c68b10984
tilt-rgb <lesma_chan@pm.me>
2025-09-18 20:15:20 +0200

    m52xq: Spoof BuildFingerprint as Pixel Beta
    * It is necessary to pass integrity with the built-in Keybox Importer, as our stock fingerprint is not "Valid"

--- lineage_m52xq.mk
+++ lineage_m52xq.mk
@@ -49,5 +49,5 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Vendor fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint="samsung/m52xqins/m52xq:11/RP1A.200720.012/M526BXXS7CYE1:user/release-keys" \
+    BuildFingerprint="google/husky_beta/husky:16/BP41.250822.010/14082742:user/release-keys" \
     BuildDesc="m52xqins-user 11 RP1A.200720.012 M526BXXS7CYE1 release-keys"
